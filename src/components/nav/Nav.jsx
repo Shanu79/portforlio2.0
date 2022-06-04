ee5138cfffe963bf525b792ef9aa5c0c364bcebb
Swagat Raj <shanu.r9971@gmail.com>
2022-06-04 12:11:57 +0530

    added portforlio section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,31 +14,31 @@ const Nav = () => {
       <a href='#' onClick={()=> setActiveNav('#')} className={activeNav==='#'?'active':''}>
         <TiHome className='nav_icons'/>
         <div>
-          <small className='text-light'>Home</small>
+          <small className='text-light-nav'>Home</small>
         </div>
       </a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav==='#about'?'active':''}>
         <FaUserCircle className='nav_icons'/>
         <div>
-          <small className='text-light'>About me</small>
+          <small className='text-light-nav'>About me</small>
         </div>
       </a>
       <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}>
         <MdSchool className='nav_icons'/>
         <div>
-          <small className='text-light'>Education & skills</small>
+          <small className='text-light-nav'>Education & skills</small>
         </div>
       </a>
       <a href='#porforlio' onClick={()=> setActiveNav('#porforlio')} className={activeNav==='#porforlio'?'active':''}>
         <MdWork className='nav_icons'/>
         <div>
-          <small className='text-light'>Projects</small>
+          <small className='text-light-nav'>Projects & Experience</small>
         </div>
       </a>
       <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}>
         <RiServiceFill className='nav_icons'/>
         <div>
-          <small className='text-light'>Contact me</small>
+          <small className='text-light-nav'>Contact me</small>
         </div>
       </a>
     </nav>
--- src/components/portforlio/Porforlio.jsx
+++ src/components/portforlio/Porforlio.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
 import './portforlio.css'
+import IMG1 from '../../assets/work1.png'
+import IMG2 from '../../assets/work2.png'
+import IMG3 from '../../assets/work3.jpg'
+import IMG4 from '../../assets/work3.jpg'
+import IMG5 from '../../assets/work3.jpg'
+import IMG6 from '../../assets/work3.jpg'
 
 const Porforlio = () => {
   return (
-    <section id='porforlio'>Porforlio</section>
+    <section id='porforlio'>
+      <h2>. . .</h2>
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+
+      <div className='container portforlio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt=""/>
+          </div>
+          <h3>Item Title</h3>
+          <div className='cta'>
+            <a href='' className='btn' target="_blank">GitHub</a>
+            <a href='' className='btn btn-primary' target="_blank">LiveDemo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt=""/>
+          </div>
+          <h3>Item Title</h3>
+          <div className='cta'>
+            <a href='' className='btn' target="_blank">GitHub</a>
+            <a href='' className='btn btn-primary' target="_blank">LiveDemo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt=""/>
+          </div>
+          <h3>Item Title</h3>
+          <div className='cta'>
+            <a href='' className='btn' target="_blank">GitHub</a>
+            <a href='' className='btn btn-primary' target="_blank">LiveDemo</a>
+          </div>
+        </article>
+      </div>
+
+      <h2>. . .</h2>
+      <h5>My</h5>
+      <h2>Experience</h2>
+      <div className='container exp__container'>
+      <div className='exp__skills'>
+          <h3>National Institute of Technology, Rourkela</h3>
+          <div className='exp__content'>
+            <article className='exp__details'>
+                <div>
+                  <h4>B.tech</h4>
+                  <h4>Electrical & Electronics Engineering</h4>
+                  <small className='text-light'>2020-present</small>
+                </div>
+            </article>
+          </div>
+        </div>
+        <div className='exp__skills'>
+          <h3>Desiger at One Solutions, Jeypore</h3>
+          <div className='exp__content'>
+            <article className='exp__details'>
+                <div>
+                  <h5>Summer Intern</h5>
+                  <h5>April 2021-June 2021</h5>
+                  <small className='text-light'>Created the main website of the kingdom of Jey‐pore. And revamped the old kingdom emblem/logo
+with modern graphic software.</small>
+                  <h5>...</h5>
+                  <small className='text-light'>Technologies used: Figma, Adobe Photoshop, Adobe
+Illustrator, Wordpress</small>
+                </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
