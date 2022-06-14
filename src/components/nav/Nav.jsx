595b49e6846bc6623bc45fb359ce860f21f10cf1
Swagat Raj <shanu.r9971@gmail.com>
2022-06-14 20:54:43 +0530

    added google icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 import './nav.css'
-import {TiHome} from 'react-icons/ti'
-import {FaUserCircle} from 'react-icons/fa'
-import {MdSchool} from 'react-icons/md'
-import {MdWork} from 'react-icons/md'
-import {RiServiceFill} from 'react-icons/ri'
 import {useState} from 'react'
 
 const Nav = () => {
@@ -12,31 +7,41 @@ const Nav = () => {
   return (
     <nav>
       <a href='#' onClick={()=> setActiveNav('#')} className={activeNav==='#'?'active':''}>
-        <TiHome className='nav_icons'/>
+      <span class="material-symbols-rounded">
+cottage
+</span>
         <div>
           <small className='text-light-nav'>Home</small>
         </div>
       </a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav==='#about'?'active':''}>
-        <FaUserCircle className='nav_icons'/>
+      <span class="material-symbols-rounded">
+face
+</span>
         <div>
           <small className='text-light-nav'>About me</small>
         </div>
       </a>
       <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}>
-        <MdSchool className='nav_icons'/>
+      <span class="material-symbols-rounded">
+school
+</span>
         <div>
           <small className='text-light-nav'>Education & skills</small>
         </div>
       </a>
       <a href='#porforlio' onClick={()=> setActiveNav('#porforlio')} className={activeNav==='#porforlio'?'active':''}>
-        <MdWork className='nav_icons'/>
+      <span class="material-symbols-rounded">
+work
+</span>
         <div>
           <small className='text-light-nav'>Projects & Experience</small>
         </div>
       </a>
       <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}>
-        <RiServiceFill className='nav_icons'/>
+      <span class="material-symbols-rounded">
+handshake
+</span>
         <div>
           <small className='text-light-nav'>Contact me</small>
         </div>
