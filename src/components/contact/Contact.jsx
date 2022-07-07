ffa38c5b0c537f9a9859dc85a699c0db8ffa7c25
Swagat Raj <shanu.r9971@gmail.com>
2022-07-07 12:24:09 +0530

    updated contact me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './contact.css';
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_v1mdn9l', 'template_o6z40ze', form.current, '0VnmSbTNoBBP88pRa')
+
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>. . .</h2>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+          <span class="material-symbols-rounded">mail</span>
+            <h4>Email</h4>
+            <h5>shanu.r9971@gamil.com</h5>
+            <a href='mailto:shanu.r9971@gmail.com' target='_blank'>Send a mail</a>
+          </article>
+          <article className='contact__option'>
+          <span class="material-symbols-rounded">chat</span>
+            <h4>WhatsApp</h4>
+            <h5>+91 8144665857</h5>
+            <a href='https://wa.me/+918144665857?text=hi' target='_blank'>Send a message</a>
+          </article>
+          </div>
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Your Full Name" required/>
+            <input type="email" name="email" placeholder='Your E-Mail' required/>
+            <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+          </form>
+        
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,53 +1,63 @@
-import React from 'react'
-import './nav.css'
-import {useState} from 'react'
+import React from "react";
+import "./nav.css";
+import { useState } from "react";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#');
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav==='#'?'active':''}>
-      <span class="material-symbols-rounded">
-cottage
-</span>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <span class="material-symbols-rounded">cottage</span>
         <div>
-          <small className='text-light-nav'>Home</small>
+          <small className="text-light-nav">Home</small>
         </div>
       </a>
-      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav==='#about'?'active':''}>
-      <span class="material-symbols-rounded">
-face
-</span>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <span class="material-symbols-rounded">face</span>
         <div>
-          <small className='text-light-nav'>About me</small>
+          <small className="text-light-nav">About me</small>
         </div>
       </a>
-      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}>
-      <span class="material-symbols-rounded">
-school
-</span>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <span class="material-symbols-rounded">school</span>
         <div>
-          <small className='text-light-nav'>Education & skills</small>
+          <small className="text-light-nav">Education & skills</small>
         </div>
       </a>
-      <a href='#porforlio' onClick={()=> setActiveNav('#porforlio')} className={activeNav==='#porforlio'?'active':''}>
-      <span class="material-symbols-rounded">
-work
-</span>
+      <a
+        href="#porforlio"
+        onClick={() => setActiveNav("#porforlio")}
+        className={activeNav === "#porforlio" ? "active" : ""}
+      >
+        <span class="material-symbols-rounded">work</span>
         <div>
-          <small className='text-light-nav'>Projects & Experience</small>
+          <small className="text-light-nav">Projects & Experience</small>
         </div>
       </a>
-      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}>
-      <span class="material-symbols-rounded">
-handshake
-</span>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <span class="material-symbols-rounded">handshake</span>
         <div>
-          <small className='text-light-nav'>Contact me</small>
+          <small className="text-light-nav">Contact me</small>
         </div>
       </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
